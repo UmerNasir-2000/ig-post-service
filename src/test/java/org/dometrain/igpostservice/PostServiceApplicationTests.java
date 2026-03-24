c7c8d585f52b97d41b3c90a1043a465a9a5bb01c
Umer Nasir <umer.nasir@razormetrics.com>
2026-03-24 12:44:25 +0500

    FEAT: Initialize Project

---
+++ src/test/java/org/dometrain/igpostservice/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dometrain.igpostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
